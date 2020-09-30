861e6bfb3d5017370fc43a3c21b5e85cf9e1be06
Your Name <you@example.com>
2020-09-30 08:58:11 +0300

    made the errors feature

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,1 +1,18 @@
-<h1>Header</h1>
+
+  <nav class="uk-navbar-container uk-margin" uk-navbar>
+      <div class="uk-navbar-left">
+
+          <a class="uk-navbar-item uk-logo" href="/"><img src="img/logo.png">  </a>
+
+         
+
+
+          <div class="uk-navbar-item">
+              <form action="javascript:void(0)">
+                  <input class="uk-input uk-form-width-small" type="text" placeholder="Input">
+                  <button class="uk-button uk-button-default">Button</button>
+              </form>
+          </div>
+
+      </div>
+  </nav>
