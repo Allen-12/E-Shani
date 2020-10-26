2c78cd0a3b6e92b49f2dc815861aa0f7463bd696
Allen-12 <allennjuguna@gmail.com>
2020-10-26 13:25:09 +0300

    Login page working

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,88 +1,69 @@
 @extends('layouts.auth')
 
 @section('content')
-<nav class="navbar navbar-color-on-scroll navbar-transparent fixed-top navbar-expand-lg bg-info"  color-on-scroll="100">
-  <div class="container">
-      <div class="navbar-translate">
-          <a href="/"><img src="{{URL::to('logo.png')}}"></a>
+    <nav class="navbar navbar-color-on-scroll navbar-transparent fixed-top navbar-expand-lg bg-info"  color-on-scroll="100">
+        <div class="container">
+            <div class="navbar-translate">
+                <a href="/"><img src="{{URL::to('logo.png')}}"></a>
 
-      </div>
-  </div>
-</nav>
-
-
-<div class="page-header header-filter" style="background-image: url('https://images.unsplash.com/photo-1453749024858-4bca89bd9edc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'); background-size: cover; background-position: top center;">
-
-<div class="container">
-  <br>
-  <br>
-  <br>
-
-      <div class="row">
-        <div class="col-lg-4 col-md-6 ml-auto mr-auto">
-          <div class="card card-login">
-            <form class="form" method="" action="">
-              <div class="card-header card-header-primary text-center">
-                <h4 class="card-body">Log In to Your E-Toto Account!</h4>
-
-
-              </div>
-              <p class="description text-center">Fill in your details..</p>
-              <div class="card-body">
-                <div class="input-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text">
-                      <i class="material-icons">face</i>
-                    </span>
-                  </div>
-                  <input type="text" class="form-control" placeholder=" Name...">
-                </div>
-                <div class="input-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text">
-                      <i class="material-icons">mail</i>
-                    </span>
-                  </div>
-                  <input type="email" class="form-control" placeholder="Email...">
-                </div>
-                <div class="input-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text">
-                      <i class="material-icons">lock_outline</i>
-                    </span>
-                  </div>
-                  <input type="password" class="form-control" placeholder="Password...">
-                </div>
-              </div>
-              <div class="footer text-center">
-                <a href="#pablo" class="btn btn-rose btn-round btn-lg">Get Started</a>
-                <a href="{{ route("register") }}" class="btn btn-primary btn-link btn-lg">Don't have an account? Sign up</a>
-              </div>
-
-
-
-            </form>
-          </div>
-
+            </div>
         </div>
-      </div>
+    </nav>
+
+
+    <div class="page-header header-filter" style="background-image: url('https://images.unsplash.com/photo-1453749024858-4bca89bd9edc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'); background-size: cover; background-position: top center;">
+
+        <div class="container">
+            <br>
+            <br>
+            <br>
+
+            <div class="row">
+                <div class="col-lg-4 col-md-6 ml-auto mr-auto">
+                    <div class="card card-login">
+                        <form class="form" method="post" action="{{ route("login") }}">
+                            @csrf
+
+                            <div class="card-header card-header-primary text-center">
+                                <h4 class="card-body">Log In to Your E-Toto Account!</h4>
+                            </div>
+                            <p class="description text-center">Fill in your details..</p>
+                            <div class="card-body">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text">
+                                          <i class="material-icons">mail</i>
+                                        </span>
+                                    </div>
+                                    <input type="email" class="form-control" name="email" placeholder="Email...">
+                                    @error('email')
+                                        <small class="text-danger">{{ $message }}</small>
+                                    @enderror
+                                </div>
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text">
+                                          <i class="material-icons">lock_outline</i>
+                                        </span>
+                                    </div>
+                                    <input type="password" class="form-control" name="password" placeholder="Password...">
+                                    @error('password')
+                                        <small class="text-danger">{{ $message }}</small>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="footer text-center">
+                                <button type="submit" class="btn btn-rose btn-round btn-lg">Login</button>
+                                <a href="{{ route("register") }}" class="btn btn-primary btn-link btn-lg">Don't have an account? Sign up</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!-- <div class="container">
+    <!-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
